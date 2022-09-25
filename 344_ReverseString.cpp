d0ddef729ecafe19fb7966b8e747b7990d7e07be
Daixin Tian <105831455+AlezHibali@users.noreply.github.com>
2022-09-25 03:29:47 -0400

    Create 344_ReverseString.cpp

---
+++ 344_ReverseString.cpp
@@ -0,0 +1,20 @@
+// 344. Reverse String
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        // if empty
+        if (s.empty())
+            return;
+        
+        int start = 0;
+        int end = s.size()-1;
+        
+        while (start <= end){
+            iter_swap(s.begin()+start, s.begin()+end);
+            // swap(s[begin],s[end]);
+            start++;
+            end--;
+        }
+    }
+};

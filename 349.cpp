4ed1888827bce1b89e9ab875904347bbbed66705
Daixin Tian <105831455+AlezHibali@users.noreply.github.com>
2022-09-24 20:39:14 -0400

    Update 349.cpp

--- 349.cpp
+++ 349.cpp
@@ -26,3 +26,21 @@ public:
         return result;
     }
 };
+
+// Solution 2
+class Solution2 {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> hash(nums1.begin(), nums1.end());
+
+        vector<int> result;
+        
+        for (int i = 0; i < nums2.size(); i++){
+            // if found, then erase and push
+            if (hash.erase(nums2[i]))
+                result.push_back(nums2[i]);
+        }
+
+        return result;
+    }
+};

9479005f1b68257c9fa3613c17d89aec09f96789
Daixin Tian <105831455+AlezHibali@users.noreply.github.com>
2022-10-01 17:30:35 -0400

    Create 242_Valid_Anagram.cpp

---
+++ 242_Valid_Anagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> st (26,0);
+        
+        for (auto i : s)
+            st[i-'a']++;
+        for (auto i : t)
+            st[i-'a']--;
+        
+        for (auto i : st)
+            if (i != 0)
+                return false;
+        
+        return true;
+    }
+};

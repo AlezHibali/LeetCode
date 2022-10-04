d8a4ec7390dea3caf068b91a26c2c3a82154c2d3
Daixin Tian <105831455+AlezHibali@users.noreply.github.com>
2022-10-04 04:14:30 -0400

    Update 88_MergeSortedArray.cpp

--- 88_MergeSortedArray.cpp
+++ 88_MergeSortedArray.cpp
@@ -1,3 +1,18 @@
+// Iteration
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        if (n == 0) return;
+        
+        while (n){
+            if (m > 0 && nums1[m-1]>nums2[n-1])
+                nums1[(m--)+n-1] = nums1[m-1];
+            else
+                nums1[m+(n--)-1] = nums2[n-1];
+        }
+    }
+};
+
 // recursion
 class Solution {
 public:
